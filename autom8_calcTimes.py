d09842b0c13391fb93be897dc5a4a8ee79e6de5d
Nathan Aherne <nathan@reddog.com.au>
2018-03-04 11:17:21 +1000

    updates:

--- autom8_calcTimes.py
+++ autom8_calcTimes.py
@@ -3,39 +3,39 @@
 import autom8_userVars
 
 # Loop through each Function OnOff setting
-for i in autom8_userVars.funcOnOffArray:
-    print("TESTING i: ", i)
+for funcSettings in autom8_userVars.funcOnOffArray:
+    print("TESTING funcSettings: ", funcSettings)
 
     # Check if functionOnOff value has plus or minus in them
-    plus = i.find("+")
+    plus = funcSettings.find("+")
     print("TESTING Plus: ", plus)
-    minus = i.find("-")
+    minus = funcSettings.find("-")
     print("TESTING Minus: ", minus) 
 
     # If there is no time settings
-    if timeSetting == "":
+    if funcSettings == "":
         startTime = ""
         endTime = ""
 
     # If function has been set to off
-    elif timeSetting == "off":
+    elif funcSettings == "off":
         startTime = ""
         endTime = ""
 
     # If function is set to run always
-    elif timeSetting == "always":
+    elif funcSettings == "always":
         startTime = "now"
         endTime = "now + 12 hours"
 
     # If function has "+" or "-" in its timesetting
     elif plus:
-        beforeAfterPlus = i.split("+")
+        beforeAfterPlus = funcSettings.split("+")
         print("TESTING beforePlus ", beforeAfterPlus[0], " AfterPlus ", beforeAfterPlus[1])
         #startTime = "before+"
         #endTime = "before+" + "after +"
         
     elif minus:
-        beforeAfterMinus = i.split("+")
+        beforeAfterMinus = funcSettings.split("+")
         print("TESTING beforeMinux ", beforeAfterMinus[0], " AfterMinus ", beforeAfterMinus[1])
         #startTime = "before-" - "after-"
         #endTime = "before-"
