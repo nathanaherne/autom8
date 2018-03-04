79f19b618eaf1568ae6507389f69b3435decdb8c
Nathan Aherne <nathan@reddog.com.au>
2018-03-04 12:17:10 +1000

    updates

--- autom8_calcTimes.py
+++ autom8_calcTimes.py
@@ -41,6 +41,7 @@ for funcN_Array in autom8_userVars.funcArray:
         print("ERROR")
 
 
+    # Calculate Start Times
     if startTime == "dawn":
         funcN_Array[2] = autom8_vars.dawn
     elif startTime == "sunrise":
@@ -51,6 +52,22 @@ for funcN_Array in autom8_userVars.funcArray:
         funcN_Array[2] = autom8_vars.sunset
     elif startTime == "dusk":
         funcN_Array[2] = autom8_vars.dawn
+    else:
+        funcN_Array[2] = startTime
+
+    # Calculate End Times
+    if endTime == "dawn":
+        funcN_Array[3] = autom8_vars.dawn
+    elif endTime == "sunrise":
+        funcN_Array[3] = autom8_vars.sunrise
+    elif endTime == "noon":
+        funcN_Array[3] = autom8_vars.noon
+    elif endTime == "sunset":
+        funcN_Array[3] = autom8_vars.sunset
+    elif endTime == "dusk":
+        funcN_Array[3] = autom8_vars.dawn
+    else:
+        funcN_Array[3] = endTime
 
 
     print("TESTING funcX_Alias: ", funcN_Array[0])
