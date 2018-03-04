5dc3f100ac9ab4654c8092ae7cbbc3ee5e09b0f7
Nathan Aherne <nathan@reddog.com.au>
2018-03-04 11:29:54 +1000

    updates

--- autom8_calcTimes.py
+++ autom8_calcTimes.py
@@ -4,46 +4,46 @@ import autom8_userVars
 
 # Loop through each Function OnOff setting
 for funcSettings in autom8_userVars.funcOnOffArray:
-    print("TESTING funcSettings: ", funcSettings)
 
     # Check if functionOnOff value has plus or minus in them
     plus = funcSettings.find("+")
-    print("TESTING Plus: ", plus)
     minus = funcSettings.find("-")
-    print("TESTING Minus: ", minus) 
 
     # If there is no time settings
     if funcSettings == "":
         startTime = "Blank"
         endTime = "Blank"
-        print("Start Time: ", startTime, "End Time: ", endTime)
 
     # If function has been set to off
     elif funcSettings == "off":
         startTime = "Blank"
         endTime = "Blank"
-        print("Start Time: ", startTime, "End Time: ", endTime)
 
     # If function is set to run always
     elif funcSettings == "always":
         startTime = "now"
-        endTime = "now + 12 hours"
-        print("Start Time: ", startTime, "End Time: ", endTime)
+        endTime = "never"
 
     # If function has "+" or "-" in its timesetting
     elif plus:
         beforeAfterOperand = funcSettings.split("+")
         startTime = beforeAfterOperand[0]
         endTime = beforeAfterOperand[1]
-        print("Start Time: ", startTime, "End Time: ", endTime)
         
     elif minus:
         beforeAfterOperand = funcSettings.split("+")
         startTime = beforeAfterOperand[0]
         endTime = beforeAfterOperand[1]
-        print("Start Time: ", startTime, "End Time: ", endTime)
 
     else:
         print("ERROR")
+`   
+    print("TESTING funcSettings: ", funcSettings)
+    if plus:
+        print("Plus Found: ", plus)
+    elif minus;
+    print("Minus Found: ", minus)
+
+    print("Start Time: ", startTime, "End Time: ", endTime)
 
 
