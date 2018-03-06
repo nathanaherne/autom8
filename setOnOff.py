54d4b1e47ef12331272063048723aeb8844ea65c
Nathan Aherne <nathan@reddog.com.au>
2018-03-06 18:40:19 +1000

    updates

--- setOnOff.py
+++ setOnOff.py
@@ -11,16 +11,16 @@ import RPi.GPIO as GPIO
 # Loop through each Function OnOff setting
 for funcN_Array, i in zip(userVars.funcArray, globalVars.outPins):
 
-	print("TESTING Function Name", type(funcN_Array[0]))
-	print("TESTING Function On/Off", type(funcN_Array[1]))
-	print("TESTING Function On", type(funcN_Array[2]))
-	print("TESTING Function Off", type(funcN_Array[3]))
+	print("TESTING Function Name", funcN_Array[0])
+	print("TESTING Function On/Off", funcN_Array[1])
+	print("TESTING Function On", funcN_Array[2])
+	print("TESTING Function Off", funcN_Array[3])
 
 	# If startTime is less than now() and startTime is not blank AND
 	# endTime > now() OR endTime is blank
-	if funcN_Array[2] != "":
+	if funcN_Array[2] != datetime.min:
 
-		if funcN_Array[2] <= datetime.utcnow() and (funcN_Array[3] > datetime.utcnow() or funcN_Array[3] == ''):
+		if funcN_Array[2] <= datetime.now(utc) and (funcN_Array[3] > datetime.now(utc) or funcN_Array[3] == datetime.max):
 
 			# Turn pin on
 			GPIO.output(i, 0)
