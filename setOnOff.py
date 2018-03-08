229cd64b882a4d4b4b5bc3f8ac44fca8268f8f95
Nathan Aherne <nathan@reddog.com.au>
2018-03-08 19:50:15 +1000

    updates

--- setOnOff.py
+++ setOnOff.py
@@ -13,14 +13,13 @@ import pytz
 for funcN_Array, i in zip(userVars.funcArray, globalVars.outPins):
 
 	print("--------------------------------------------------")
-	print(str(funcN_Array[0]))
+	print(str(funcN_Array[0]), "On/Off setting is: ", funcN_Array[1])
 
 	# If startTime is less than now() and startTime is not blank AND
 	# endTime > now() OR endTime is blank
 	if funcN_Array[2] != datetime.min:
 
 		if funcN_Array[2] <= datetime.utcnow() and (funcN_Array[3] > datetime.utcnow() or funcN_Array[3] == datetime.max) :
-
 			# Turn pin on
 			GPIO.output(i, 0)
 			print("Currently on - turned on at", str(funcN_Array[2]))
