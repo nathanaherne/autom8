caecca769b2a709f256bce4b7bdffab3f7fdcf5d
Nathan Aherne <nathan@reddog.com.au>
2018-03-12 17:00:26 +1000

    updates

--- resetPins.py
+++ resetPins.py
@@ -2,17 +2,11 @@
 
 # Libraries
 import RPi.GPIO as GPIO
-import time
-import pytz
-from datetime import datetime, timedelta
 
 GPIO.setmode(GPIO.BCM)
 
 # Import program variables
 import globalVars
-
-# Import user variables
-import userVars
 
 ##########################################################
 # SETUP
@@ -22,12 +16,3 @@ for i in globalVars.outPins:
 
         GPIO.setup(i, GPIO.OUT) 
         GPIO.output(i, 1)
-
-# Get Sun/Moon stages
-#import calcSunMoon
-
-# Calculate on/off times
-#import calcOnOff
-
-# Set on/off times
-#import setOnOff
