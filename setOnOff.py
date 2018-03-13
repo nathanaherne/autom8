b96ff1a23a87679f0ba236e8d72763008bd6aa8b
Nathan Aherne <nathan@reddog.com.au>
2018-03-13 10:21:49 +1000

    updates

--- setOnOff.py
+++ setOnOff.py
@@ -19,9 +19,9 @@ for funcN_Array, i in zip(userVars.funcArray, globalVars.outPins):
 
 	# If startTime is less than now() and startTime is not blank AND
 	# endTime > now() OR endTime is blank
-	if funcN_Array[2] != datetime.min:
+	if funcN_Array[2] != dt.datetime.min:
 
-		if funcN_Array[2] <= datetime.utcnow() and (funcN_Array[3] > datetime.utcnow() or funcN_Array[3] == datetime.max) :
+		if funcN_Array[2] <= dt.datetime.utcnow() and (funcN_Array[3] > dt.datetime.utcnow() or funcN_Array[3] == dt.datetime.max) :
 			# Turn pin on
 			GPIO.setup(i, GPIO.OUT) 
 			GPIO.output(i, 0)
